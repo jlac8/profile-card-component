c9972e5480cd3e1d3fbae8a95872fb1bd5e91c8f
Juan <jlalvac@uni.pe>
2024-09-10 14:23:11 -0500

    feat: add HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,30 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      Cristian Moreno
-      33
-      London
-
-      80K
-      Followers
-
-      803K
-      Likes
-
-      1.4K
-      Photos
-    </>
+    <main>
+      <section>
+        <div>
+          <img src="" alt="" />
+          <p>Juan Alva</p>
+          <span>23</span>
+          <p>London</p>
+        </div>
+        <div>
+          <div>
+            80K
+            Followers
+          </div>
+          <div>
+            803K
+            Likes
+          </div>
+          <div>
+            1.4K
+            Photos
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
