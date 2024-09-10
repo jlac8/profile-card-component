ca9305c9afe52f8f52150b71418b691a31bb7e87
Juan <jlalvac@uni.pe>
2024-09-10 14:14:02 -0500

    feat: add input files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Cristian Moreno
+      33
+      London
+
+      80K
+      Followers
+
+      803K
+      Likes
+
+      1.4K
+      Photos
+    </>
+  )
+}
+
+export default App
