2688e675efd8ef114a513dcdd2624d1b428b9b4e
Juan <jlalvac@uni.pe>
2024-09-10 21:40:20 -0500

    feat: add styles

--- src/components/DemographicInfo.jsx
+++ src/components/DemographicInfo.jsx
@@ -2,8 +2,10 @@ function DemographicInfo() {
   return (
     <div className="profile-card__demographic">
       <img className="profile-card__picture" src="\assets\image-profile.jpg" alt="Image Profile" />
-      <p className="profile-card__name">Juan Alva</p>
-      <span className="profile-card__age">23</span>
+      <p>
+        <span className="profile-card__name">Victor Crest</span>
+        <span className="profile-card__age">26</span>
+      </p>
       <p className="profile-card__city">London</p>
     </div>
   )
